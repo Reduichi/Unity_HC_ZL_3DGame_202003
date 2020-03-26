b51f78852c2f28aa840bdce6a9e1097e146cc9cb
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-03-26 20:08:31 +0800

    練習靜態方法

--- 3D Game/Assets/Scripts/LearnCSharp.cs
+++ 3D Game/Assets/Scripts/LearnCSharp.cs
@@ -1,8 +1,25 @@
-﻿// 引用Unity Engine API的窗口 (窗口 : 開發人員的倉庫)
+﻿// 引用 Unity Engine API (窗口：開發人員用的倉庫)
 using UnityEngine;
 
 // 類別  腳本名稱
 public class LearnCSharp : MonoBehaviour
 {
-// 括號一定成對出現
+    // C# 內括號一定成對：() [] {} "" '' 
+    // 類別 : 成員
+    // 1. 屬性 (欄位、屬性 Prop)
+    // 2. 方法 (功能)
+    // 3. 事件 (Unity) - 特定時間執行的方法
+
+    // 開始事件 : 播放遊戲時執行一次
+    private void Start()
+    {
+        print("嗨~");
+    }
+
+    // 更新事件 : 每一秒執行約60次
+    private void Update()
+    {
+        print("哈哈哈");
+    }
+
 }
---
+++ 3D Game/Assets/Scripts/LearnMember.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LearnMember : MonoBehaviour
+{
+    // 定義屬性
+    // 語法 : 類型 名稱 指定 值;
+    float a = 0.5f;
+    int b = 70;
+    bool c = true, d = false;
+    string e = "我是字串";
+
+    // 修飾詞 類型 名稱 指定 值;
+    // 公開 public 允許所有文件存取 : 顯示在屬性面板
+    public int score = 10;
+    // 私人 private 僅限此類別存取 : 隱藏在屬性面板
+    private int speed = 99;
+
+    void Start()
+    {
+        // 使用靜態成員
+        // 靜態屬性 Static Properties
+        // 取得 Get : 類別.靜態屬性
+        print(Random.value);
+        // 設定 Set : 類別.靜態屬性 = 值
+        // 不能設定 Read Only 唯讀的屬性
+        Time.timeScale = 0.5f;  // 讓時間變慢
+
+        // 使用靜態方法
+        print(Random.Range(0.1f, 99.9f));   // 第一個多載
+        print(Random.Range(100, 500));      // 第二個多載
+
+    }
+}
