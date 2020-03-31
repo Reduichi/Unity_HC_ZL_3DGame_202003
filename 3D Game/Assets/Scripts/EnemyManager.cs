b4995a7ad96c49562e4532906ab5137c85076399
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-03-31 21:34:17 +0800

    練習屬性欄位

---
+++ 3D Game/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Enemy enemy1, enemy2;
+
+    private void Start()
+    {
+        // 取得 欄位
+        print(enemy1.hp);
+        // 設定 欄位
+        enemy2.hp = 200;
+
+        // 設定 屬性
+        enemy1.attack = 999;
+        // 取得 屬性
+        print("怪物1號的攻擊力:" + enemy1.attack);
+    }
+}
