4eef228a4bd68ce8258395460179311cc3722236
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-03-31 20:05:16 +0800

    0.331

---
+++ 3D Game/Assets/Scripts/ExerciseAPI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ExerciseAPI : MonoBehaviour
+{
+    public SpriteRenderer kid;
+    public Transform cam;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+
+        kid.flipX = true;
+
+        print(Mathf.Floor(1.23456f));
+
+        cam.Rotate(0 , 90 , 0);
+    }
+}
