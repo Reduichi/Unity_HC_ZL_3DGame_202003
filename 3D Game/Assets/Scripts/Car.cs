9cec05d4e350bf90efa6a00ec8ffe5f3d6e0e280
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-04-07 19:55:10 +0800

    方法

---
+++ 3D Game/Assets/Scripts/Car.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // 方法語法 : 修飾詞 傳回類型 方法名稱(參數) {敘述}
+    // void 無傳回 : 使用此方法時不會有任何資料傳回
+    /// <summary>
+    /// 開車
+    /// </summary>
+    public void Drive()
+    {
+        print("開車");
+    }
+}
--- 3D Game/Assets/Scripts/EnemyManager.cs
+++ 3D Game/Assets/Scripts/EnemyManager.cs
@@ -15,5 +15,16 @@ public class EnemyManager : MonoBehaviour
         enemy1.attack = 999;
         // 取得 屬性
         print("怪物1號的攻擊力:" + enemy1.attack);
+
+        // 設定屬性 : 設定唯讀屬性會造成錯誤
+        // enemy2.def = 1;
+        // 取得屬性
+        print("怪物 2 號的攻擊力 :" + enemy2.def);
+        enemy2.lv = 99;
+        print("怪物 2 號的MP :" + enemy2.mp);
+
+        enemy2.damage = 10;
+        print("對怪物 2 造成的傷害 :" + enemy2.damage);
+
     }
 }
