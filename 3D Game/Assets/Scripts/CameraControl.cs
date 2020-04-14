1d2bf9b1f7dbbc99c9cd01917be26ebe01d3ba4a
Reduichi <57396815+Reduichi@users.noreply.github.com>
2020-04-14 21:34:10 +0800

    追蹤

---
+++ 3D Game/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform dragon;
+    [Header("追蹤速度"), Range(0, 100)]
+    public float speed;
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        Vector3 draPos = dragon.position;
+        draPos.y = 60;
+        draPos.z -= 5;
+        transform.position = Vector3.Lerp(transform.position, draPos, 0.3f * Time.deltaTime * speed);
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
